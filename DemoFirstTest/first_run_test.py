9ee1c922e5ec90504ffaa3e7b6f4dfe9026691f7
atrox3d <atrox3d@gmail.com>
2024-07-08 07:07:56 +0200

    add driver fixture, headless

--- DemoFirstTest/first_run_test.py
+++ DemoFirstTest/first_run_test.py
@@ -1,25 +1,33 @@
+import pytest
 from selenium import webdriver
 
-def test_lambdatest_playground():
-  driver = webdriver.Chrome()
+@pytest.fixture
+def driver():
+  options = webdriver.ChromeOptions()
+  options.add_argument("--headless=new")
+  driver = webdriver.Chrome(options=options)
+  return driver
+
+def test_lambdatest_playground(driver):
+  # driver = webdriver.Chrome()
   driver.maximize_window()
   driver.get("https://www.lambdatest.com/selenium-playground/")
   print("Title: ", driver.title)
 
-def test2_lambdatest_ecommerce():
-  driver = webdriver.Chrome()
+def test2_lambdatest_ecommerce(driver):
+  # driver = webdriver.Chrome()
   driver.maximize_window()
   driver.get("https://ecommerce-playground.lambdatest.io/")
   print("Title: ", driver.title)
 
-def testRexWebsite():
-  driver = webdriver.Chrome()
+def testRexWebsite(driver):
+  # driver = webdriver.Chrome()
   driver.maximize_window()
   driver.get("https://rexjones2.com")
   print("Title: ", driver.title)
 
-def google_test():
-  driver = webdriver.Chrome()
+def google_test(driver):
+  # driver = webdriver.Chrome()
   driver.maximize_window()
   driver.get("https://google.com")
   print("Title: ", driver.title)
