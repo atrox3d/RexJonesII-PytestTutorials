6232079075b1338fd93295f300854119abd32122
atrox3d <atrox3d@gmail.com>
2024-07-08 12:43:08 +0200

    add fixture

--- DemoPytest/test_multiple_subset_form.py
+++ DemoPytest/test_multiple_subset_form.py
@@ -2,13 +2,21 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def test_lambdatest_simple_form_demo():
-  driver = webdriver.Chrome()
-  driver.maximize_window()
+
+@pytest.fixture
+def driver():
+  options = webdriver.ChromeOptions()
+  options.add_argument("--headless=new")
+  driver = webdriver.Chrome(options=options)
+  return driver
+
+def test_lambdatest_simple_form_demo(driver):
+  # driver = webdriver.Chrome()
+  # driver.maximize_window()
   driver.get("https://www.lambdatest.com/selenium-playground/simple-form-demo")
-  driver.find_element(By.XPATH,
-    "//input[@id='user-message']")\
-  .send_keys("Pytest Is A Test Framework")
+
+  xpath = "//input[@id='user-message']"
+  driver.find_element(By.XPATH, xpath).send_keys("Pytest Is A Test Framework")
   driver.find_element(By.ID, "showInput").click()
   message = driver.find_element(By.ID, "message").text
   assert message == "Pytest Is A Test Framework"
