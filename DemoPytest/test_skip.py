a1ad89bc3651996f2a11725a3282e91529ae05d4
atrox3d <atrox3d@gmail.com>
2024-07-15 10:14:01 +0200

    add comments, explanations

--- DemoPytest/test_skip.py
+++ DemoPytest/test_skip.py
@@ -6,13 +6,19 @@ from selenium import webdriver
 
 class TestLambdaTest:
   def test_sample_app_title(self):
+    #
+    # skip directly
+    #
+    pytest.skip()
     driver = webdriver.Chrome()
     driver.get("https://lambdatest.github.io/sample-todo-app/")
-    pytest.skip()
     expected_title = "Sample page - lambdatest.com"
     assert expected_title == driver.title
 
   @pytest.mark.skip(reason="Code Has Not Been Deployed")
+  #
+  # skip with decorator and reason
+  #
   def test_ecommerce_title(self):
     driver = webdriver.Chrome()
     driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=product/category&path=17")
@@ -20,6 +26,9 @@ class TestLambdaTest:
     assert expected_title == driver.title
 
   @pytest.mark.skip()
+  #
+  # unconditional skip with decorator w/o reason
+  #
   def test_special_offers(self):
     driver = webdriver.Chrome()
     driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=product/special")
@@ -29,6 +38,9 @@ class TestLambdaTest:
   @pytest.mark.skipif(
       datetime.now() <= datetime(2099, 12, 31),
       reason="Repo Is Not Complete Until After Finishing Tutorial")
+  #
+  # conditional skip with decorator and reason
+  #
   def test_pytest_github_repo(self):
     driver = webdriver.Chrome()
     driver.get("https://github.com/RexJonesII/PytestTutorials")
