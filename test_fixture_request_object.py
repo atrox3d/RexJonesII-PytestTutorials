6551346d14baa55468a2418dbc19b6e4dc497df8
atrox3d <atrox3d@gmail.com>
2024-07-15 08:15:09 +0200

    add more detailed output and docstrings

--- test_fixture_request_object.py
+++ test_fixture_request_object.py
@@ -3,34 +3,46 @@ import attr
 import pytest
 
 def print_obj(obj):
+    ''' prints attributes of object merging __dict__ and then dir() '''
+
+    obj_name = obj.__class__.__name__
     names = set()
     names.update(list(obj.__dict__))
     names.update(att for att in dir(obj) if not '__' in att)
     for name in sorted(names):
         try:
-            print(f'print_obj | obj[{name}] = {obj.__dict__[name]}')
+            print(f'print_obj | {obj_name}[{name}] = {obj.__dict__[name]}')
         except KeyError:
-            print(f'print_obj | obj.{name} = {getattr(obj, name)}')
+            print(f'print_obj | {obj_name}.{name} = {getattr(obj, name)}')
 
 @pytest.fixture(params=['test-one-param', 2])
 def get_class_request(request):
-    print(f'get_request | {id(request) = }')
-    print(f'get_request | {request.param = }')
+    ''' adds attributes to the client class '''
+
+    print(f'get_class_request | {id(request) = }')
+    print(f'get_class_request | {request.param = }')
     #
     # inject values inside calling class
     #
-    print(request.cls)
+    print(f'get_class_request | {request.cls = }')
+    print(f'get_class_request | setting {request.cls.__name__}.id = {id(request.function)}')
     request.cls.id = id(request.function)
+    print(f'get_class_request | setting request.id = {id(request)}')
     request.id = id(request)
 
 @pytest.mark.usefixtures("get_class_request")
 class TestFixture:
     def test_fixture(self):
-        print(self.id)
+        ''' this will be called two times, one for each param in get_class_request '''
+        #
+        # class is modified by get_class_request
+        #
+        print(f'TestFixture::test_fixture | {self.id = }')
         print_obj(self)
 
 @pytest.fixture(params=['test-one-param', 2])
 def get_function_request(request):
+    ''' returns the request object created from params in fixture '''
     print(f'get_function_request | {id(request) = }')
     print(f'get_function_request | {request.param = }')
     #
